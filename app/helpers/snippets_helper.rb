668a5b6db27771c6fa811ab0000ade4fff0ca0fb
a_bahaa <ahmedbahaa_11@hotmail.com>
2018-04-11 13:37:58 +0200

    using redis

--- app/helpers/snippets_helper.rb
+++ app/helpers/snippets_helper.rb
@@ -1,2 +1,11 @@
 module SnippetsHelper
+  def fetch_snippets
+    snippets = $redis.get("snippets")
+    if snippets.nil?
+      snippets = Snippet.all.to_json
+      $redis.set("snippets",snippets)
+      $redis.expire("snippets",5.hour.to_i)
+    end
+    JSON.load snippets
+  end
 end
